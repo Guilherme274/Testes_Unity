b5ef5eab26a9abaaecc736c1bdfafb6e4d4230d7
Guilherme Paixão dos Santos <paixaoguilherme79@gmail.com>
2023-11-07 22:01:40 -0300

    Adicionando Animação de Agachar e Atacar

--- Teste_Unity/Assets/Scripts/Player.cs
+++ Teste_Unity/Assets/Scripts/Player.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using TMPro;
 using UnityEngine.SceneManagement;
 using Cinemachine;
+using UnityEditor.SceneManagement;
 
 public class Player : MonoBehaviour
 {
@@ -22,6 +23,8 @@ public class Player : MonoBehaviour
     [SerializeField] Vector2 spawn;
     [SerializeField] GameObject prefabRobo;
     CinemachineVirtualCamera cineCamera;
+    bool agachou = false;
+
 
        private void Awake()
     {
@@ -40,37 +43,53 @@ public class Player : MonoBehaviour
     {
         anim.SetFloat("posicaoY", rb.velocity.y);
         vertical = transform.position.y;
-        if(Input.GetButtonDown("Jump") && noChao)
+        if (agachou == false)
         {
-            rb.AddForce(Vector2.up * forcaPulo);
-            
-            anim.SetBool("pulou", true);
-
-            noChao = false;
-
-        }
-        else
-        {
-            if(Input.GetButtonDown("Jump") && noChao == false && pulou == false)
+            if (Input.GetButtonDown("Jump") && noChao)
             {
                 rb.AddForce(Vector2.up * forcaPulo);
 
                 anim.SetBool("pulou", true);
 
-                pulou = true;
+                noChao = false;
+
+            }
+            else
+            {
+                if (Input.GetButtonDown("Jump") && noChao == false && pulou == false)
+                {
+
+                    rb.AddForce(Vector2.up * forcaPulo);
+
+                    anim.SetBool("pulou", true);
+
+                    pulou = true;
+                }
             }
         }
+        var posicaoX = rb.velocity.x;
 
-        if(horizontal >0)
+        if (horizontal > 0)
         {
+            agachou = false;
             lado = 1;
+            Agachar();
         }
-        if(horizontal < 0)
+        if (horizontal < 0)
         {
+            agachou = false;
             lado = -1;
+            Agachar();
+        }
+        if (Input.GetMouseButtonDown(0))
+        {
+            anim.SetBool("atacou", true);
+            Debug.Log("atacou");
         }
 
-        if(lado != 0)
+        //anim.SetBool("atacou", false);
+
+        if (lado != 0)
         {
             transform.localScale = new Vector2(lado, transform.localScale.y);
         }
@@ -80,11 +99,26 @@ public class Player : MonoBehaviour
         //    mensagem.SetActive(true);
         //}
         
+        if(Input.GetKeyDown(KeyCode.C))
+        {
+            agachou = true;
+            Agachar();
+        }
 
-            horizontal = Input.GetAxisRaw("Horizontal");
+        if (Input.GetKeyUp(KeyCode.C) && agachou)
+        {
+            agachou = false;
+            Agachar();
+
+        }
+
+        
+
+        horizontal = Input.GetAxisRaw("Horizontal");
 
        
-         anim.SetFloat("posicaoX", Mathf.Abs(rb.velocity.x));
+         anim.SetFloat("posicaoX", Mathf.Abs(posicaoX));
+        
         
         
         if(morreu)
@@ -142,5 +176,9 @@ public class Player : MonoBehaviour
         //mensagem.SetActive(false);
     }
 
+    public void Agachar()
+    {
+        anim.SetBool("agachou", agachou);
+    }
 
 }
